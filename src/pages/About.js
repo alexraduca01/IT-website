9216a1b68cb4c1dc87d8cad0f40f8e79dcbf891c
alexraduca01 <alex.raduca01@icloud.com>
2024-04-19 15:39:27 +0200

    created more pages

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      Sono About
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Contacts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contacts = () => {
+  return (
+    <div>
+      Sono Contacts
+    </div>
+  )
+}
+
+export default Contacts
