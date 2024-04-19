b5d7e8822fa0627ff93d54c676c13ed90bd90252
alexraduca01 <alex.raduca01@icloud.com>
2024-04-19 14:48:18 +0200

    created starting components/pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+// import vari
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// componenti 
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// pagine
+import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return(
+    <Router className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
 };
 
 export default App;
