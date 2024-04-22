7f96fa17c3ad59d787eb199ab102e8847ca7bf4e
alexraduca01 <alex.raduca01@icloud.com>
2024-04-22 17:38:02 +0200

    added initial form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -45,6 +45,105 @@ const Home = () => {
             </div>
           </div>
         </section>
+        {/* sezione form per preventivo  */}
+        <section className="w-full p-10">
+          <div className="w-4/5 m-auto bg-slate-100 pb-10 px-8">
+            <h2 className="text-3xl text-center py-10">Contattaci</h2>
+            <div className="w-full flex justify-between">
+              <form className="w-1/2 m-auto p-5 bg-white rounded-md flex flex-col gap-5">
+                <div className="flex gap-3 justify-between">
+                  <div className="w-1/2">
+                    <label
+                      for="first_name"
+                      className="block mb-2 text-sm font-medium text-gray-900 "
+                    >
+                      Nome <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      id="first_name"
+                      className="bg-gray-50 border text-sm rounded-lg block w-full p-2.5  border-gray-600 placeholder-gray-400  focus:ring-blue-500 focus:border-blue-500"
+                      required
+                    />
+                  </div>
+                  <div className="w-1/2">
+                    <label
+                      for="last_name"
+                      className="block mb-2 text-sm font-medium text-gray-900 "
+                    >
+                      Cognome <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      id="last_name"
+                      className="bg-gray-50 border text-sm rounded-lg block w-full p-2.5  border-gray-600 placeholder-gray-400  focus:ring-blue-500 focus:border-blue-500"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="w-full">
+                  <label
+                    for="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Email <span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="bg-gray-50 border text-sm rounded-lg block w-full p-2.5  border-gray-600 placeholder-gray-400  focus:ring-blue-500 focus:border-blue-500"
+                    required
+                  />
+                </div>
+                <div className="w-full">
+                  <label
+                    for="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Phone
+                  </label>
+                  <input
+                    type="text"
+                    id="text"
+                    className="bg-gray-50 border text-sm rounded-lg block w-full p-2.5  border-gray-600 placeholder-gray-400  focus:ring-blue-500 focus:border-blue-500"
+                  />
+                </div>
+                <div className="w-full">
+                  <label
+                    for="message"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Message <span className="text-red-500">*</span>
+                  </label>
+                  <textarea
+                    rows={4}
+                    type="text"
+                    id="message"
+                    className="bg-gray-50 border text-sm rounded-lg block w-full p-2.5  border-gray-600 placeholder-gray-400  focus:ring-blue-500 focus:border-blue-500"
+                  />
+                </div>
+                <div className="w-full flex justify-center gap-10">
+                  <button
+                    type="submit"
+                    className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+                  >
+                    Invia
+                  </button>
+                  <button
+                    type="reset"
+                    className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"
+                  >
+                    Annulla
+                  </button>
+                </div>
+              </form>
+              <div className="w-1/2 ps-10">
+                <p className="text-xl mb-5">Oppure chiamaci al</p>
+                <a href="tel:+393333333333" className="text-3xl text-green-700 hover:text-green-300 transition duration-150 ease-linear">+39 333 333 3333</a>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
